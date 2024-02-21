ba8eec204cd4cdd2c74027216c6b0f5aa6662439
EvgheniAdvahov <evghenii.88@gmail.com>
2024-02-21 12:09:31 +0200

    HW3_2 Comparable

---
+++ HW3_2/src/main/java/org/example/WorkerList.java
@@ -0,0 +1,22 @@
+package org.example;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.List;
+import java.util.function.Consumer;
+
+public class WorkerList implements Iterable<BasicWorker>{
+    private  BasicWorker[] myWorkerLists = {
+            new Freelancer("Alexandr", 45, "Cahul", 500),
+   new Worker("Gregory", 35, "London", 85000),
+   new Freelancer("Sergey", 19, "Moscow", 450),
+   new Worker("Evgheni", 36, "Seatle", 300000)
+    };
+
+
+    @Override
+    public Iterator<BasicWorker> iterator() {
+        return Arrays.asList(myWorkerLists).iterator();
+    }
+}
