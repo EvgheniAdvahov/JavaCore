61e69b27f8ab3d2b05e592c0bfcd9e31ba09680e
EvgheniAdvahov <evghenii.88@gmail.com>
2024-02-21 11:14:29 +0200

    HW3_2 Comparable

---
+++ HW3_2/src/main/java/org/example/Main.java
@@ -0,0 +1,28 @@
+package org.example;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class Main {
+
+    public static void main(String[] args) {
+        List<BasicWorker> workerList = new ArrayList<>();
+        BasicWorker worker = new Freelancer("Alexandr", 45, "Cahul", 500);
+        BasicWorker worker1 = new Worker("Gregory", 35, "London", 85000);
+        BasicWorker worker2 = new Freelancer("Sergey", 19, "Moscow", 450);
+        BasicWorker worker3 = new Worker("Evgheni", 36, "Seatle", 300000);
+
+        workerList.add(worker);
+        workerList.add(worker1);
+        workerList.add(worker2);
+        workerList.add(worker3);
+
+
+        System.out.println(workerList);
+        //Sort by age
+        Collections.sort(workerList);
+        System.out.println(workerList);
+
+    }
+}
