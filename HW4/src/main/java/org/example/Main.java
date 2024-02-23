32a0580436697f4b86cf34616ea74324c65cf90a
EvgheniAdvahov <evghenii.88@gmail.com>
2024-02-23 18:03:07 +0200

    mvp

--- HW4/src/main/java/org/example/Main.java
+++ HW4/src/main/java/org/example/Main.java
@@ -12,21 +12,11 @@ public class Main {
         Account account = new Account("Evgheni", "44-55-666", 23000);
         Account account1 = new Account("Sergey", "55-66-777", 35000);
         Account account2 = new Account("Vladimir", "77-88-999", 5000);
+        Account account3 = new Account("Test", "77-88-999",  -5000);
 
-//        Operation opeartion = new Depozit(account, -5000);
+        Operation opeartion = new Depozit(account, -5000);
         Operation opeartion1 = new Withdrawal(account, 100000);
 
-        for (Operation bankOperation : OperationList.operationList) {
-            try{
-                OperationService.operationCheck(bankOperation);
-            } catch (InsuficientFundsException e) {
-                e.printStackTrace();
-            } catch (IllegalArgumentException e) {
-                e.printStackTrace();
-            }
-        }
-
-        System.out.println(AccountList.accountList);
 
         for (Account bankaccount : AccountList.accountList) {
             try {
@@ -37,5 +27,14 @@ public class Main {
         }
 
 
+        for (Operation bankOperation : OperationList.operationList) {
+            try{
+                OperationService.operationCheck(bankOperation);
+            } catch (InsuficientFundsException e) {
+                e.printStackTrace();
+            } catch (IllegalArgumentException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
--- HW4/src/main/java/org/example/Service/AccountCreationService.java
+++ HW4/src/main/java/org/example/Service/AccountCreationService.java
@@ -4,8 +4,9 @@ import org.example.Account;
 
 public class AccountCreationService {
     public static void checkAccount(Account account) throws IllegalArgumentException {
-        if(account.getBalance() < 0)
-            throw new IllegalArgumentException("Баланс аккаунта " + account.getName() + " не может быть отрицательным " + account.getBalance());
+        if(account.getBalance() < 0) {
+            throw new IllegalArgumentException( "Баланс аккаунта " + account.getName() + " не может быть отрицательным " + account.getBalance());
+        }
     }
 
 }
