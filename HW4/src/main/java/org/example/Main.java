9ad4214f4fd779918899dea9e3bb8af6b939bdd3
EvgheniAdvahov <evghenii.88@gmail.com>
2024-02-25 11:11:08 +0200

    Transaction exception

--- HW4/src/main/java/org/example/Main.java
+++ HW4/src/main/java/org/example/Main.java
@@ -4,6 +4,7 @@ import org.example.MyException.InsuficientFundsException;
 import org.example.Service.Operations.Depozit;
 import org.example.Service.Operations.Operation;
 import org.example.Service.Operations.Withdrawal;
+import org.example.Service.Transaction;
 
 public class Main {
 
@@ -13,6 +14,7 @@ public class Main {
         Account account2 = null;
         Account account3 = null;
 
+        //Account exception
         try {
             account = Account.create("Evgheni", "44-55-666", 23000);
             account1 = Account.create("Sergey", "55-66-777", 35000);
@@ -21,11 +23,21 @@ public class Main {
             e.printStackTrace();
         }
 
-
+        //Operation exceptions
         try {
             Operation operation1 = Withdrawal.createWithdraw(account1, 1000000);
             Operation operation2 = Depozit.createDepozit(account1, -5000);
         } catch (InsuficientFundsException e) {
+            e.printStackTrace();
+        }
+
+        //Transaction exceptions
+        try {
+        Transaction.SendMoney(account, account1, 30000);
+        Transaction.SendMoney(account, account1, -3000);
+        } catch (InsuficientFundsException e) {
+            e.printStackTrace();
+        } catch (IllegalArgumentException e){
             e.printStackTrace();
         }
 
